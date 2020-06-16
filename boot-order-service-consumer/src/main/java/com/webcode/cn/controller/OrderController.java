dd323e5bd50b9385ec6b95a9d9ec3f300acf6b20
wushaopei <471979809@qq.com>
2020-06-16 20:56:53 +0800

    springboot整合dubbo，为服务消费者

---
+++ boot-order-service-consumer/src/main/java/com/webcode/cn/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.webcode.cn.controller;
+
+import java.util.List;
+
+import com.webcode.cn.bean.UserAddress;
+import com.webcode.cn.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class OrderController {
+	
+	@Autowired
+	OrderService orderService;
+	
+	@ResponseBody
+	@RequestMapping("/initOrder")
+	public List<UserAddress> initOrder(@RequestParam("uid")String userId) {
+		return orderService.initOrder(userId);
+	}
+
+}
---
+++ boot-order-service-consumer/src/test/java/com/webcode/cn/BootOrderServiceConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcode.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootOrderServiceConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
